126ffe1d693c5e76b8abdfdf3dae2bc5e3cceccf
Hans <741581879@qq.com>
2020-08-28 12:37:20 +0800

    [rank][Hans] test: Add voyageRisk case 1. should return 1 when voyageRisk given voyage length is 4 test

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,10 +1,11 @@
 const rankTest = require('ava');
+const {voyageRisk} = require('../src/rank');
 
-rankTest('foo', t => {
-  t.pass();
-});
-
-rankTest('bar', async t => {
-  const bar = Promise.resolve('bar');
-  t.is(await bar, 'bar');
+rankTest('voyageRisk case 1. should return 1 when voyageRisk given voyage length is 4', t => {
+    const voyage = {
+        zone: 'west-indies',
+        length: 4,
+    };
+    const result = voyageRisk(voyage);
+    t.is(result, 1);
 });
