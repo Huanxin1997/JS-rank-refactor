d88b701f8c69166c6d22f64376ff61bb04430201
Hans <741581879@qq.com>
2020-08-28 19:58:56 +0800

    [rank][Hans] refactor: Extract printDetails method

--- src/print.js
+++ src/print.js
@@ -1,20 +1,28 @@
+function printBanner() {
+    console.log('***********************');
+    console.log('**** Customer Owes ****');
+    console.log('***********************');
+}
+
+function printDetails(invoice, outstanding) {
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
 function printOwing (invoice) {
-  let outstanding = 0;
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
+    let outstanding = 0;
+    printBanner();
 
-  // calculate outstanding
-  for (const o of invoice.borderSpacing) {
-    outstanding += o.amount;
-  }
+    // calculate outstanding
+    for (const o of invoice.borderSpacing) {
+        outstanding += o.amount;
+    }
 
-  // record due date
-  const today = new Date();
-  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+    // record due date
+    const today = new Date();
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
-  // print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
+    // print details
+    printDetails(invoice, outstanding);
 }
